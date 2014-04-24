253c4e0f40056f1a198ded1c4b9ecb7e4f71fe9a
Kevin Aguet <kevin.aguet@gmail.com>
2014-04-24 10:52:46 -0700

    Sexy main menu with tweening

--- Assets/Scripts/UI/StartScreenUI.cs
+++ Assets/Scripts/UI/StartScreenUI.cs
@@ -1,25 +1,53 @@
 ﻿using UnityEngine;
-using System.Collections;
+using System.Collections.Generic;
 
-public class StartScreenUI : MonoBehaviour {
-
-	[SerializeField] private GameObject _playButton;
+public class StartScreenUI : MonoBehaviour 
+{
+	[SerializeField] private UILabel _titleLabel;
+	[SerializeField] private List<GameObject> _menuButtons;
 
 	public delegate void EventDelegate();
 	public event EventDelegate PlayPressed; 
 
 	public void Init()
 	{
-		TweenInButtons();
+		_titleLabel.gameObject.SetActive(false);
+		foreach(GameObject menuButton in _menuButtons)
+		{
+			menuButton.SetActive(false);
+		}
+
+		Invoke("TweenInTitleLabel", 0.5f);
+		Invoke("TweenInButtons", 1.5f);
+	}
+
+	private void TweenInTitleLabel()
+	{
+		_titleLabel.gameObject.SetActive(true);
+
+		iTween.ScaleFrom(_titleLabel.gameObject, iTween.Hash(
+			"scale", new Vector3(60f, 60f, 1f),
+			"easetype", "easeOutElastic",
+			"time", .28f
+		));
 	}
 
 	private void TweenInButtons()
 	{
-		iTween.MoveFrom(_playButton, iTween.Hash(
-			"x", -2f,
-			"time", 0.8f,
-			"easetype", "easeOutBounce"
-		));
+		for(int i = 0; i < _menuButtons.Count; i++)
+		{
+			GameObject menuButton = _menuButtons[i];
+
+			menuButton.SetActive(true);
+			float tweenDelay = i*0.2f;
+
+			iTween.MoveFrom(menuButton, iTween.Hash(
+				"x", -3.5f,
+				"time", 0.8f,
+				"easetype", "easeOutBounce",
+				"delay", tweenDelay
+			));
+		}
 	}
 
 	private void OnPlayPressed()
